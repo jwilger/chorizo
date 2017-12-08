7b35e8d1f40aa6916b71b45a0127997146407262
John Wilger <john@johnwilger.com>
2017-12-08 11:10:06 -0800

    Adds docs for Schema

--- lib/chorizo_core/entities/schema.ex
+++ lib/chorizo_core/entities/schema.ex
@@ -1,4 +1,12 @@
 defmodule ChorizoCore.Entities.Schema do
+  @moduledoc """
+  Use this to add Ecto Schemas to entities in this project.
+
+  Instead of putting `use Ecto.Schema` in an entity, use the code `use
+  ChorizoCore.Entities.Schema` instead. Doing so will ensure that all entities
+  are using UUIDs for their primary key fields.
+  """
+
   defmacro __using__(_) do
     quote do
       use Ecto.Schema
